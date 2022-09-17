4738e727b52bcc7a12e801c342e0be55b491ac84
Andreu Mut <andrac275@gmail.com>
2022-09-17 13:25:28 +0200

    Objetos en JS. Son como diccionarios de python

---
+++ 15_JavaScript_DeclaracionVariables y TiposDeDatos/03_objetosEnJS.js
@@ -0,0 +1,19 @@
+//Objetos en JavaScript
+//Un objeto es una agrupacion de datos que hacen sentido entre si
+
+//Objeto vacio
+const miPrimerObjeto = {}
+
+//Objeto
+const miObjeto = {
+    //Esto de dentro del objeto se llama propiedad
+    //prpiedad 1
+    unNumero: 12,
+    //prpiedad 2
+    unString: 'cadena de caracteres',
+    //propiedad 3
+    condicion: true,
+}
+
+console.log('Todo el objeto', miObjeto)
+console.log('Una unica propiedad: ', miObjeto.unString)
