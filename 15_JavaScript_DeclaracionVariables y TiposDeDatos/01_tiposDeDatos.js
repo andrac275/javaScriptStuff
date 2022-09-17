e961b5e8284cd94040689afe847370873279fc6a
Andreu Mut <andrac275@gmail.com>
2022-09-17 12:47:42 +0200

    Tipos de datos de JS

---
+++ 15_JavaScript_DeclaracionVariables y TiposDeDatos/01_tiposDeDatos.js
@@ -0,0 +1,9 @@
+//TIPOS DE DATOS de JS:
+
+// String: Cadena de caracteres. Entre comillas simples o comillas dobles.
+// Boolean: true o false.
+// Null: es un valor nulo, hace referencia a una variable vacia, que no contiene datos.
+// Number: 12312 123.123.   Cuidado que 123 no es lo mismo que "123"
+// Undefined: PArecido a nulo. Null es una variable DEFINIDA con valor nulo, mientras que 
+//     Undefined es una variable que NO ha sido definida
+// Object: objeto. Son estructuras que permiten agrupar los tipos de datos anteriores.
