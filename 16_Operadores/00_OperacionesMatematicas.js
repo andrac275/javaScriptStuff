6f694fe3150ca58bcba14e7359bfeab11fec6a7d
Andreu Mut <andrac275@gmail.com>
2022-09-17 13:46:32 +0200

    Operadores matematicos de JS

---
+++ 16_Operadores/00_OperacionesMatematicas.js
@@ -0,0 +1,22 @@
+//Operaciones matematicas en JS
+
+const suma = 1 + 2
+const restar = 1 - 2
+const multiplicar = 2 * 3
+const dividir = 9 / 3
+const modulo = 10 % 3
+console.log('Mostrar suma, resta, multipl, division y modulo: ',suma, restar, multiplicar, dividir, modulo)
+console.log('-------------')
+let numero = 5
+numero++
+numero--
+console.log('Incrementar y decrementar en 1 una variable (mira codigo): ', numero)
+
+numero += 5
+console.log('Incrementar en 5 un numero (mira codigo): ', numero)
+numero -= 5
+console.log('Decrementar en 5 un numero (mira codigo): ', numero)
+numero *= 5
+console.log('Multiplicar por 5 un numero (mira codigo): ', numero)
+numero /= 5
+console.log('Dividir entre 5 un numero (mira codigo): ', numero)
