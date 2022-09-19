2f0b021572fcfa743c77e62502b0f6a2aeaa14ae
Andreu Mut <andrac275@gmail.com>
2022-09-19 12:29:02 +0200

    JS; Operadores de comparacion

---
+++ 16_Operadores/01_OperadoresDeComparacion.js
@@ -0,0 +1,20 @@
+//Operadores Comparacion
+console.log('5 === 6' , 5 === 6)
+console.log('5 == 5' , 5 == 5)
+//Los strings son iguales a los numeros si el valor es el mismo
+console.log('5 == "5" (string)' , 5 == '5')
+//Pero con la igualdad estricta, da false.
+console.log('5 === "5"' , 5 === '5')
+
+console.log('5 < 6' , 5 < 6)
+console.log('5 < 5' , 5 < 5)
+console.log('5 <= 5' , 5 <= 5)
+
+//Desigualdad
+console.log('5 !== 6', 5 !== 6)
+console.log('5 != "5"' , 5 != '5')
+
+
+
+
+
